b26fbbcaf4a5c816b2a7648aaa5fe8c121d1886c
Vladimir Br <1693357+Vladimirbr@users.noreply.github.com>
2020-12-26 18:08:47 +0200

    setup for notification microservice

---
+++ notification-microservice/src/app.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import httpErrors from "http-errors";
+import logger from "morgan";
+import dotenv from "dotenv";
+
+import indexRouter from "./routes/index";
+
+const app: express.Application = express();
+
+// ENV var config
+dotenv.config();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// routes
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+  next(httpErrors(404));
+});
+
+// error handler
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json(err);
+});
+
+export default app;
---
+++ notification-microservice/src/config/config.ts
@@ -0,0 +1,2 @@
+export const PORT = process.env.PORT;
+export const CRUD_SERVER_URL = process.env.CRUD_SERVER_URL;
